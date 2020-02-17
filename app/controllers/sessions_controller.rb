055470003cc5a2638fc0d95d1be857f6bc8d2b3f
nicholasykng <nicholasykng@gmail.com>
2020-02-17 09:37:53 -0800

    solving auth issues if else

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,23 @@ class SessionsController < ApplicationController
     end
 
     def create
-            if @user = User.find_or_create_by(name: auth['name']) do |u|
+        if auth
+            @user = User.find_or_create_by(name: auth['info']['name']) do |u|
                 u.email = auth['info']['email']
+                u.password = SecureRandom.hex
             end
-            session[:user_id] = @user.id
-            redirect_to "/"
-            else
-                @user = User.find_by(email: params[:email])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+        else
+            @user = User.find_by(email: params[:email])
                 if @user.authenticate(params[:password])
                     session[:user_id] = @user.id 
                     redirect_to user_path(@user)
                 else
                     render :new
                 end
-            end
+        end
+            
     end
 
     def destroy
