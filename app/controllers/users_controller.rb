669e31d37d2416da85089cb04c6eab4fefbeddf0
nicholasykng <nicholasykng@gmail.com>
2020-02-16 20:30:42 -0800

    finalize sign up page and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(users_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to "/"
+        else
+            render :new
+        end
+    end
+
+    private
+    def users_params
+        params.require(:user).permit(:name, :email, :password)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :comments
     has_many :ratings
+    has_secure_password
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :email, presence: true
+    validates :password, presence: true
 end
