2027744aae7b0d5d16662782338d2682f5e77c92
nicholasykng <nicholasykng@gmail.com>
2020-02-16 21:06:23 -0800

    finalize logging in and out plus sign ups

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,15 @@ class SessionsController < ApplicationController
         @user = User.find_by(email: params[:email])
         if @user.authenticate(params[:password])
             session[:user_id] = @user.id 
-            redirect_to "user_path(@user)"
+            redirect_to user_path(@user)
         else
             render :new
         end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to "/"
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "/signup" => 'users#new'
   post "/signin" => 'users#create'
-  resources :users
+  resources :users, only: [:show]
   get "/login" => 'sessions#new'
   post "/login" => 'sessions#create'
+  get "/logout" => 'sessions#destroy'
+  root "users#index"
 end
