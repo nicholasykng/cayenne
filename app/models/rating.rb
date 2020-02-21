4ce37983d866567e00380561c19f8aa075464c96
nicholasykng <nicholasykng@gmail.com>
2020-02-20 19:09:34 -0800

    finalize validation for rating not accept number not from 0-10

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
-    validates :score, numericality: '{ less_than_or_equal_to :10 }'
+    validates :score, numericality: { less_than_or_equal_to: 10 }
+    validates :score, numericality: { greater_than_or_equal_to: 0 }
     validates :score, presence: true
 end
