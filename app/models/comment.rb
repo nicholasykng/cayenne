d38e278b358f104f22f019b849d2aa74fc3ec142
nicholasykng <nicholasykng@gmail.com>
2020-02-20 20:20:32 -0800

    final validtion checks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
     validates :content, presence: true
+    validates :recipe_id, uniqueness: { scope: :user_id } #User can only comment once on the recipe
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,9 @@
 class Rating < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
-    validates :score, numericality: { less_than_or_equal_to: 10 }
+    validates :score, numericality: { less_than_or_equal_to: 10 } 
     validates :score, numericality: { greater_than_or_equal_to: 0 }
+    validates :score, numericality: { only_integer: true }
     validates :score, presence: true
+    validates :recipe_id, uniqueness: { scope: :user_id} #User can only rate once on the recipe
 end
